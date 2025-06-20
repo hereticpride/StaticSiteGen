737491944f6a7abebdde7d0ffdb34a4161a21723
hereticpride <allensmalls32@gmail.com>
2025-06-20 11:51:49 -0400

    added markdown to blocks function

--- src/delimiter.py
+++ src/delimiter.py
@@ -81,3 +81,11 @@ def text_to_textnode(text):
 
     return link_txt
 
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    result = []
+    for block in blocks:
+        stripped_block = block.strip()
+        if stripped_block != "":
+            result.append(stripped_block)
+    return result
--- src/test_delimiter.py
+++ src/test_delimiter.py
@@ -104,4 +104,25 @@ class TestDelimiter(unittest.TestCase):
         text = ""
         txt_to_node = text_to_textnode(text)
         print(f" empty string: {txt_to_node}")
+
+    def test_markdown_to_blocks(self):
+        md = """
+This is **bolded** paragraph
+
+
+This is another paragraph with _italic_ text and `code` here
+This is the same paragraph on a new line
+
+- This is a list
+- with items
+"""
+        blocks = markdown_to_blocks(md)
+        self.assertEqual(
+            blocks,
+            [
+                "This is **bolded** paragraph",
+                "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
+                "- This is a list\n- with items",
+            ],
+        )
         
